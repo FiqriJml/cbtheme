b5b20f3e7210f815b6cb37c38e47da527e41d2d2
fiqrijml <jamalfiqri@gmail.com>
2024-07-21 12:07:59 +0800

    add menu

--- header.php
+++ header.php
@@ -7,4 +7,20 @@
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+    <header class="site-header">
+        <div class="site-branding">
+            <h1 class="site-title"><?php bloginfo('name'); ?></h1>
+        </div><!-- .site-branding -->
+
+        <nav class="main-navigation">
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'primary',
+                    'menu_id' => 'primary-menu',
+                )
+            );
+            ?>
+        </nav><!-- .main-navigation -->
+    </header><!-- .site-header -->
