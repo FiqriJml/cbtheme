9286553c6639c094426d43aab217d507f91b5c35
fiqrijml <jamalfiqri@gmail.com>
2024-07-21 11:59:17 +0800

    register menu

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -3,5 +3,11 @@
 function mytheme_setup()
 {
     add_theme_support('title-tag');
+    // Daftarkan lokasi menu
+    register_nav_menus(
+        array(
+            'primary' => __('Primary Menu', 'mytheme'),
+        )
+    );
 }
 add_action('after_setup_theme', 'mytheme_setup');
