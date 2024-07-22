70768ef00343613d144734b9dd6ab4b95e2ba89e
fiqrijml <jamalfiqri@gmail.com>
2024-07-22 20:55:49 +0800

    add block editor

--- inc/custom-block-editor.php
+++ inc/custom-block-editor.php
@@ -1,9 +1,40 @@
 <?php
-use Carbon_Fields\Container;
+use Carbon_Fields\Block;
 use Carbon_Fields\Field;
+use Carbon_Fields\Container;
 
-function crb_attach_theme_options()
+
+add_action('carbon_fields_register_fields', 'custom_block');
+function custom_block()
 {
+    // Hero section
+    Block::make(__('Hero'))
+        ->add_fields(
+            array(
+                Field::make('text', 'heading', __('Block Heading')),
+                Field::make('image', 'image', __('Block Image')),
+                Field::make('rich_text', 'content', __('Block Content')),
+            )
+        )
+        ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
+            ?>
 
+        <div class="block">
+            <div class="block__heading">
+                <h1><?php echo esc_html($fields['heading']); ?></h1>
+            </div><!-- /.block__heading -->
+
+            <div class="block__image">
+                <?php echo wp_get_attachment_image($fields['image'], 'full'); ?>
+            </div><!-- /.block__image -->
+
+            <div class="block__content">
+                <?php echo apply_filters('the_content', $fields['content']); ?>
+            </div><!-- /.block__content -->
+        </div><!-- /.block -->
+
+        <?php
+        });
+
+    // @end Hero
 }
-add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
