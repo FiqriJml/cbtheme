5106ac670b87782f879d314cf05abc4a43238bc1
fiqrijml <jamalfiqri@gmail.com>
2024-07-25 06:45:25 +0800

    create hero mobile block

--- components/hero.php
+++ components/hero.php
@@ -3,5 +3,11 @@ $data = getData();
 ?>
 
 <div class="hero">
-
+    <h2><span class="highlight">WordPress</span> themes and fully <span class="highlight">managed</span> websites.</h2>
+    <h3>We do everything so you don’t have to.</h3>
+    <div class="tasks">
+        <span><i class="fas fa-check"></i>Free Domain</span>
+        <span><i class="fas fa-check"></i>Free Website Migration</span>
+        <span><i class="fas fa-check"></i>24/7 Customer Support</span>
+    </div>
 </div>
