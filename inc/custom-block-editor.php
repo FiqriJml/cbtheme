671c20de48b51ff5d1a6516afec36882de95e603
fiqrijml <jamalfiqri@gmail.com>
2024-07-26 00:46:09 +0800

    dynamic data hero mobile

--- inc/custom-block-editor.php
+++ inc/custom-block-editor.php
@@ -8,16 +8,52 @@ add_action('carbon_fields_register_fields', 'custom_block');
 function custom_block()
 {
     // Hero Mobile Section
-    Block::make(__('Hero'))
+    Block::make(__('Hero Mobile'))
+        ->set_category('custom-blocks', __('Custom Blocks'), 'star-filled')
         ->add_fields(
             array(
-                Field::make('text', 'Heading', __('Heading Title'))
+                Field::make('text', 'heading', __('Heading Title')),
+                Field::make('text', 'highlight_words', __('Words to Highlight'))
+                    ->help_text(__('Separate words with commas')),
+                Field::make('complex', 'tasks', __('List We Do'))
+                    ->add_fields(
+                        array(
+                            Field::make('text', 'task', __('Task'))
+                        )
+                    ),
+                Field::make('image', 'image', __('Hero Image')),
+                Field::make('text', 'button_text', __('Text Button')),
+                Field::make('text', 'button_url', __('URL Button')),
+                Field::make('text', 'reviews_count', __('Reviews Count'))
+                    ->help_text(__('Enter the number of reviews'))
             )
-        )->set_icon('admin-appearance')
+        )->set_icon('star-filled')
         ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
+            $heading = $fields['heading'];
+            $highlight_words = $fields['highlight_words'];
+
+            // Convert the highlighted words into an array
+            $highlight_words_array = array_map('trim', explode(',', $highlight_words));
+
+            // Function to highlight words
+            function highlight_text($text, $words)
+            {
+                foreach ($words as $word) {
+                    if (!empty($word)) {
+                        $text = str_replace($word, "<span class='highlight'>{$word}</span>", $text);
+                    }
+                }
+                return $text;
+            }
+            // Apply highlighting
+            $heading = highlight_text($heading, $highlight_words_array);
+
+            $fields['heading'] = $heading;
+            $fields['image'] = wp_get_attachment_image_url($fields['image'], 'full');
             setData($fields);
             get_template_part('components/hero');
         });
+    // @end Hero
     // CTA Wizard Banner section
     Block::make(__('CTA Wizard Banner'))
         ->add_fields(
